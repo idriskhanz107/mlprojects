454308f31fba1bd865353f79a32266309c78ce7b
idriskhanz107 <idriskhan18701@gmail.com>
2024-10-17 02:54:45 +0530

    The fourth commit

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error,error_detail:sys):
@@ -10,7 +10,6 @@ def error_message_detail(error,error_detail:sys):
  
     return error_message
     
-
 class CustomException(Exception):
     def __init__(self, error_message,error_detail:sys):
         super().__init__(error_message)
